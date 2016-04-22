723bd9eecadd8739bf3f57202ddaad22adebad62
Annie <matoukaa@gmail.com>
2016-04-21 19:36:53 -0600

    email setup/contact and thank you form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email, 
+  		:to => 'matoukaa@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  		:body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
-  get 'static_pages/thank_you'
+  post 'static_pages/thank_you'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
