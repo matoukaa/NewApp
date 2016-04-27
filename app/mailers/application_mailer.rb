3a0b3ddf22e5eecf7deb3ebe0f7f231990036671
Annie <matoukaa@gmail.com>
2016-04-26 21:05:30 -0600

    Email upon sign in, homepage image change

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "matoukaa@gmail.com"
+  default from: "from@gmail.com"
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,18 @@
 class UserMailer < ApplicationMailer
-	default from: "matoukaa@gmail.com"
+	default from: "from@gmail.com"
 
 	def contact_form(email, name, message)
-  	@message = message
-  		mail(:from => @email, 
-  		:to => 'matoukaa@gmail.com',
-  		:subject => "A new contact form message from #{@name}")
-  end
+  		@message = message
+  		mail( :from => @email, 
+  			:to => 'your-email@example.com',
+  			:subject => "A new contact form message from #{@name}")
+  	end
+
+  	def welcome(user)
+  		@appname = "estore"
+  		mail( :to => user.email,
+  			:subject => "Welcome to #{@appname}!")
+  	end
+
+
 end
