8615a041082edd4b7ea716727cd2a810ad0b9cce
Annie <matoukaa@gmail.com>
2016-04-27 17:54:15 -0600

    controller (functional) tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registrations => "user_registrations" }
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
 
   resources :products do
     resources :comments
@@ -7,20 +7,16 @@ Rails.application.routes.draw do
   resources :users
 
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
   get 'static_pages/index'
-
   get 'static_pages/landing_page'
-
   post 'static_pages/thank_you'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
+  root 'products#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+	before do
+		@user = User.create!(email: "testing@gmail.com", password: "testtest")
+		@user2 = User.create!(email: "test2@gmail.com", password: "testtest2")
+	end
+
+	describe "GET #show" do
+		context "If user is logged in" do
+			before do
+				sign_in @user
+			end
+
+			it "loads correct user details of User" do
+				get :show, id: @user
+				expect(response.status).to eq 200
+				expect(assigns(:user)).to eq @user
+			end
+
+			it "won't load incorrect details of user 2" do
+				get :show, id: @user2
+				expect(response.status).to eq 302
+				expect(response).to redirect_to(root_path)
+			end
+		end
+
+		context "No user is logged in" do
+			it "redirects to login" do
+				get :show, id: @user
+				expect(response).to redirect_to(root_path)
+			end
+		end
+		
+	end
+end
